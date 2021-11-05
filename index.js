635c4a67a609a5d35484f354b0abc4dcc7fba030
JastinXyz <jastinlinggar@gmail.com>
2021-11-05 15:51:00 +0700

    ✨ Prettier

--- index.js
+++ index.js
@@ -1,118 +1,130 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
-const Swal = require('sweetalert2')
+var bodyParser = require("body-parser");
+const Swal = require("sweetalert2");
+const Collect = require("@supercharge/collections");
+const db = require("quick.db");
 
 app.use(bodyParser());
-app.set("json spaces", 2)
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-
-const db = require('quick.db')
+app.set("json spaces", 2);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 function hasWhiteSpace(s) {
-  return (/\s/).test(s);
+  return /\s/.test(s);
 }
 
 var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
-var checkForSpecialChar = function(string){
- for(i = 0; i < specialChars.length;i++){
-   if(string.indexOf(specialChars[i]) > -1){
-       return true
+var checkForSpecialChar = function (string) {
+  for (i = 0; i < specialChars.length; i++) {
+    if (string.indexOf(specialChars[i]) > -1) {
+      return true;
     }
- }
- return false;
-}
+  }
+  return false;
+};
 
+let counting = db.fetch(`shorted`);
 
-let counting = db.fetch(`shorted`)
+setInterval(function () {
+  counting = db.fetch(`shorted`);
+}, 5000);
 
-setInterval(function() {
-  counting = db.fetch(`shorted`)
-}, 5000)
+/* ----- MONGODB ----- */
+var monk = require("monk");
 
-const Collect = require('@supercharge/collections')
+const thedb = monk(process.env.MONGODB);
 
- /* ----- MONGODB ----- */
-var monk = require('monk')
+thedb
+  .then(() => {
+    console.log("[ MONGODB ] Connected to mongo server.");
+  })
+  .catch((e) => {
+    console.log("[ MONGODB ] Failed to connect to server.");
+  });
 
-const thedb = monk(process.env.MONGODB)
+const inidb = thedb.get("codenime-short");
+/* ----- END MONGODB ----- */
 
-thedb.then(() => {
-        console.log('[ MONGODB ] Connected to mongo server.')
-    })
-    .catch((e) => {
-        console.log("[ MONGODB ] Failed to connect to server.")
-    })
+app.get("/", async (req, res) => {
+  res.render("index", {
+    notavailable: false,
+    aliasnull: false,
+    deleted: false,
+  });
+});
 
-const inidb = thedb.get('codenime-short')
- /* ----- END MONGODB ----- */
+app.get("/test", async (req, res) => {
+  res.render("test", { isAdded: true, long: "test", alias: "testing" });
+});
 
- app.get('/', async(req, res) => {
+app.get("/delete", async (req, res) => {
+  res.render("remove", { aliasnull: false });
+});
 
-  res.render('index', { notavailable : false, aliasnull : false, deleted : false })
+app.get("/:short", async (req, res) => {
+  const queryshort = req.params.short;
 
-})
+  const final = inidb.findOne({ shorted: req.params.short }).then((docs) => {
+    if (docs == null) res.status(404).send("404");
+    else res.redirect(docs.url);
+  });
+});
 
-app.get('/test', async(req, res) => {
-  
-res.render('test', { isAdded : true, long: "test", alias: "testing" })
-})
+app.post("/short", async (req, res) => {
+  var urlnya = req.body.url;
+  var shortnya = req.body.short;
 
-app.get('/delete', async(req, res) => {
-  
-res.render('remove', { aliasnull : false })
-})
+  const inisudahada = await inidb.findOne({ shorted: shortnya });
 
-app.get('/:short', async(req, res) => {
-const queryshort = req.params.short
+  if (inisudahada) {
+    res.render("index", {
+      notavailable: true,
+      aliasnull: false,
+      deleted: false,
+    });
+  } else if (hasWhiteSpace(shortnya) === true) {
+    res.render("index", {
+      notavailable: true,
+      aliasnull: false,
+      deleted: false,
+    });
+  } else if (checkForSpecialChar(shortnya) === true) {
+    res.render("index", {
+      notavailable: true,
+      aliasnull: false,
+      deleted: false,
+    });
+  } else {
+    const pushtodb = {
+      url: urlnya,
+      shorted: shortnya,
+    };
 
-   const final = inidb.findOne({shorted: req.params.short}).then((docs) => {
-  if (docs == null) res.status(404).send("404")
-  else res.redirect(docs.url)
-})
-  
-})
+    inidb.insert(pushtodb),
+      res.render("test", { isAdded: true, long: urlnya, alias: shortnya }),
+      db.add("shorted", 1);
+  }
+});
 
-app.post('/short', async(req, res) => {
-   var urlnya = req.body.url;
-   var shortnya = req.body.short;
-
-const inisudahada = await inidb.findOne({shorted: shortnya})
-
-if(inisudahada) {
- res.render('index', { notavailable : true, aliasnull : false, deleted : false })
-} else if(hasWhiteSpace(shortnya) === true) {
- res.render('index', { notavailable : true, aliasnull : false, deleted : false })
-} else if(checkForSpecialChar(shortnya) === true) {
-  res.render('index', { notavailable : true, aliasnull : false, deleted : false })
-} else {
-
-const pushtodb = {
-  url: urlnya,
-  shorted: shortnya
-}
-
-inidb.insert(pushtodb), res.render('test', { isAdded : true, long: urlnya, alias: shortnya }), db.add("shorted", 1)
-      
-}
-
-})
-
-app.post('/remove', async (req, res) => {
+app.post("/remove", async (req, res) => {
   var codeshort = req.body.shortcode;
 
-  const check = await inidb.findOne({shorted: codeshort})
+  const check = await inidb.findOne({ shorted: codeshort });
 
-  if(check == null) {
-    res.render('remove', { aliasnull : true })
+  if (check == null) {
+    res.render("remove", { aliasnull: true });
   } else {
-    inidb.findOneAndDelete({shorted: codeshort}), res.render('index', { deleted : true, aliasnull : false, notavailable : false })
+    inidb.findOneAndDelete({ shorted: codeshort }),
+      res.render("index", {
+        deleted: true,
+        aliasnull: false,
+        notavailable: false,
+      });
   }
-})
+});
 
-
-app.get('/count', async (req, res) => {
+/*app.get('/count', async (req, res) => {
   res.status(200).json({
     data: {
       count: {
@@ -120,11 +132,8 @@ app.get('/count', async (req, res) => {
       }
     }
   })
-})
-
-
-
+})*/
 
 const listener = app.listen(3000, () => {
-  console.log("[ EXPRESS ] Your app listen on port " + listener.address().port)
-})
+  console.log("[ EXPRESS ] Your app listen on port " + listener.address().port);
+});
